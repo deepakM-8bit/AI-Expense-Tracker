40c7d83d8d55b258545eb1d29b1a6730eb8c782a
Deepak M <deepakm.code@gmail.com>
2025-11-09 20:34:37 +0530

    added expense UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffectEvent,useState } from "react";
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const API = 'https://localhost:3000/api/expenses';
+const App = () =>{
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <label>title
+          <input 
+          type="text"          
+          placeholder="Title of the expense"
+          />
+        </label>
+
+        <label>title
+          <input 
+          type="text"          
+          placeholder="Title of the expense"
+          />
+        </label>
+
+        <label>title
+          <input 
+          type="text"          
+          placeholder="Title of the expense"
+          />
+        </label>
+
+        <label>title
+          <input 
+          type="text"          
+          placeholder="Title of the expense"
+          />
+        </label>
+
+        <label>title
+          <input 
+          type="text"          
+          placeholder="Title of the expense"
+          />
+        </label>
+
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
