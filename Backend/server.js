71cc53e05f610fe26adfbd760064f1aa0885988d
Deepak M <deepakm.code@gmail.com>
2025-11-11 19:26:21 +0530

    added the signup and login routers and middleware to protect routers

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import expensesRouter from './Routers/expensesRouter.js';
+import authRouter from './Routers/authRouter.js';
 
 const PORT = 3000;
 const app = express();
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/auth",authRouter);
 app.use("/api/expenses",expensesRouter);
 
 app.listen(PORT, ()=>{
